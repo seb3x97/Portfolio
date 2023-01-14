98ee82726aeeb0fd99bda4b85f57b6d9d3f9db09
Sébastien Voide <sebastien.voide@eduvaud.ch>
2023-01-15 00:36:13 +0100

    J'ai enlevé des fichiers par défauts

--- exemples/TheWelcome.vue
+++ exemples/TheWelcome.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import WelcomeItem from "./WelcomeItem.vue";
+import WelcomeItem from "@//WelcomeItem.vue";
 import DocumentationIcon from "./icons/IconDocumentation.vue";
 import ToolingIcon from "./icons/IconTooling.vue";
 import EcosystemIcon from "./icons/IconEcosystem.vue";
-import CommunityIcon from "./icons/IconCommunity.vue";
+import CommunityIcon from "../src/components/icons/IconCommunity.vue";
 import SupportIcon from "./icons/IconSupport.vue";
 </script>
 
