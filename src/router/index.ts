0930668ab0527c85b6894749885b24f516d5685b
Sébastien Voide <sebastien.voide@eduvaud.ch>
2023-01-22 20:01:27 +0100

    clean

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from '@/views/HomeView.vue';
-import WorkView from '@/views/WorkView.vue';
-import EditorView from '@/views/EditorView.vue';
-import ContactView from '@/views/ContactView.vue';
 
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,22 +8,6 @@ const router = createRouter({
 			path: "/",
 			name: "home",
 			component: HomeView,
-		},
-		{
-			path: "/work",
-			name: "work",
-			component: WorkView,
-		},
-		{
-			path: "/editor",
-			name: "editor",
-			component: EditorView,
-		},
-		{
-			path: "/contact",
-			name: "contact",
-			component: ContactView,
-			//component: () => import("@/views/ContactView.vue"),
 		},
 	],
 });
